9168edb4f787cadb925a3ebc282b7636b19cdbc8
lucasfernande <lucas.fernandes6192@outlook.com>
2020-10-27 17:17:10 -0300

    registrando chamados

---
+++ registra_chamado.php
@@ -0,0 +1,21 @@
+<?php 
+
+	# tirando qualquer cerquilha dos textos para não perder a sequência
+	foreach ($_POST as $indice => $campos) {
+		$_POST[$indice] = str_replace('#', ' ', $_POST[$indice]);
+	}
+
+	# o parâmetro 'a' quer dizer que vamos abrir um arquivo apenas para escrita
+	$arquivo = fopen('chamados.txt', 'a+'); 
+
+	# juntando o conteúdo dos campos em uma string separados por uma cerquilha
+	$texto = implode('#', $_POST) . PHP_EOL; # o PHP_EOL quebra uma linha, evitando que todos os chamados fiquem numa linha só 
+	
+	# escrevendo o conteúdo dos campos no arquivo
+	fwrite($arquivo, $texto);
+
+	# fechando o arquivo
+	fclose($arquivo);
+
+	header('Location: abrir_chamado.php');
+?>
